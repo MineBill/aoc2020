8e067984daaa1bee646d8b70aabe87f7837e30cf
Minebill <terrestrialstudio@gmail.com>
2020-12-07 12:14:26 +0200

    day1

---
+++ src/day1.rs
@@ -0,0 +1,20 @@
+pub fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let input = std::fs::read_to_string("data/day1.txt")?;
+
+    let nums = input.lines().map(|s| s.parse::<i32>().unwrap()).collect::<Vec<i32>>();
+
+    for (i, curr) in nums.iter().enumerate() {
+        for next in i..nums.len() {
+            for third in next..nums.len() {
+                let next = nums.get(next).unwrap();
+                let third = nums.get(third).unwrap();
+                if curr + next + third == 2020 {
+                    println!("{}", curr * next * third);
+                    break;
+                }
+            }
+        }
+    }
+
+    Ok(())
+}
