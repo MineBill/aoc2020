2903125fece7cf093b298db982178add864350ef
Minebill <terrestrialstudio@gmail.com>
2020-12-07 17:12:02 +0200

    day3 part two

--- src/day3.rs
+++ src/day3.rs
@@ -1,23 +1,33 @@
 use crate::Result;
 
-pub fn main() -> Result {
-    let input = std::fs::read_to_string("data/day3.txt")?;
-    let mut trees = 0;
-    let mut x = 0;
-
-    for line in input.lines().skip(1) {
+fn find_trees(input: &String, x_step: usize, y_step: usize) -> i64 {
+    let mut x = 0usize;
+    let mut trees = 0i64;
+    for line in input.lines().step_by(y_step) {
         let chars = line.chars().collect::<Vec<char>>();
-        let width = line.len();
-        x += 3;
-        x = x % width;
 
         let loc = chars.get(x).unwrap();
         if *loc == '#' {
             trees += 1;
         }
-    }
 
-    println!("{}", trees);
+        let width = line.len();
+        x += x_step;
+        x = x % width;
+    }
+    trees
+}
+
+pub fn main() -> Result {
+    let input = std::fs::read_to_string("data/day3.txt")?;
+
+    let first = find_trees(&input, 1, 1);
+    let second = find_trees(&input, 3, 1);
+    let third = find_trees(&input, 5, 1);
+    let fourth = find_trees(&input, 7, 1);
+    let fifth = find_trees(&input, 1, 2);
+
+    println!("{}", first * second * third * fourth * fifth);
 
     Ok(())
 }
